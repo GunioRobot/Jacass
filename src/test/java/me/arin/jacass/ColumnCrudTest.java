6360d6777cae2cdca2d98d8f42088ac0f9214caf
Arin Sarkissian <arin@digg.com>
2010-03-24 18:27:10 -0700

    column crud test stub

---
+++ src/test/java/me/arin/jacass/ColumnCrudTest.java
@@ -0,0 +1,57 @@
+package me.arin.jacass;
+
+import junit.framework.TestCase;
+
+/**
+ * User: Arin Sarkissian
+ * Date: Mar 24, 2010
+ * Time: 6:25:55 PM
+ */
+public class ColumnCrudTest extends TestCase {
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void testGetInt() throws Exception {
+    }
+
+    public void testGetFloat() throws Exception {
+    }
+
+    public void testGetDouble() throws Exception {
+    }
+
+    public void testGetBoolean() throws Exception {
+    }
+
+    public void testGetByte() throws Exception {
+    }
+
+    public void testGetChar() throws Exception {
+    }
+
+    public void testGetLong() throws Exception {
+    }
+
+    public void testGetShort() throws Exception {
+    }
+
+    public void testGetString() throws Exception {
+    }
+
+    public void testGetRaw() throws Exception {
+    }
+
+    public void testSet() throws Exception {
+    }
+
+    public void testRemove() throws Exception {
+    }
+
+    public void testExists() throws Exception {
+    }
+}
